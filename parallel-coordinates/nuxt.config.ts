c263736937acfdeccb45825b487260150cec963d
Ismat Maarouf <ismatmaarouf19@gmail.com>
2024-11-26 12:24:59 +0400

    testing 500 error on refresh

--- parallel-coordinates/nuxt.config.ts
+++ parallel-coordinates/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
-
+  ssr: false,
   modules: ["@pinia/nuxt", "@nuxt/ui", "@nuxthub/core"],
   // extends: ["@nuxt/ui-pro"],
   colorMode: {
